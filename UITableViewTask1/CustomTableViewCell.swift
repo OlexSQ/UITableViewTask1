ac8a5aa67d85b787230b85bb2f07c359894b9389
Oleg <olegpsiarovsky@gmail.com>
2019-02-23 14:11:52 +0200

    Create simple project

---
+++ UITableViewTask1/CustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  UITableViewTask1
+//
+//  Created by mac on 23.02.2019.
+//  Copyright © 2019 Oleg_Psiarovsky. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+
+}
--- UITableViewTask1/ViewController.swift
+++ UITableViewTask1/ViewController.swift
@@ -8,13 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
+    @IBOutlet weak var tableView: UITableView!
+    private var data: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 0...1000 {
+            data.append("\(i)")
+        }
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier") as! CustomTableViewCell
+        
+        let text = data[indexPath.row]
+        
+        cell.label.text = text
+        
+        if indexPath.row % 2 == 0 {
+            cell.label.backgroundColor = UIColor.red
+        } else {
+            cell.label.backgroundColor = UIColor.lightGray
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50.0
+    }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let alertController = UIAlertController(title: "Hint", message: "You have selected row \(indexPath.row).", preferredStyle: .alert)
+        
+        let alertAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        
+        alertController.addAction(alertAction)
+        
+        present(alertController, animated: true, completion: nil)
+        
+    }
+    
 }
 
